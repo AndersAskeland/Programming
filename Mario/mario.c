922165c9082ab43527197771791ad620c4387f1d
AndersAskeland <anders_askeland@hotmail.com>
2020-01-26 14:27:36 +0100

    folder changes

--- Mario/mario.c
+++ Mario/mario.c
@@ -1,57 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 main()
-{
+{ // define integers and ask for promt
 	int i;
 	int j;
 	int rows;
-	
-	do
-	{
-		// Asks for a promt - Size
-		printf("Height: ");
-		scanf(" %d", &rows);
-	}
+  int k;
 
 
-	// Will run this prompt while n is bellow 1 - No characters etc
-	while (rows < 1);
+  do // This says that the scanf cannot recieve anything below 0
+  {
+  // Asks for a promt - Size
+	printf("This is a Mario Pyramid builder!\nEnter a number betwen 1-23: ");
+	scanf(" %d", &rows);    
+  } 
+    
+  while (rows < 1 || rows > 23); // Do the above as long as rows is bellow 1 or above 24
 
-	// Loop - creates an integer i - Counts up till i is no longer below n (as given by get_int) - Each time add one to i
-	for (i = 0; i < rows; i++)
-	{
-		// Loop - Counts up till J is no longer below i
-		for (j = 0; j < i; j = j+0)
-		{
-			printf("# ");
-		}
-		// This changes line each time - We therefore get the shape
-		printf("  \n");
-	}
+
+    // I LOOP - This creates a loop where 1 is added to i as long as i is below rows - Loops the entire bottom thing
+    for (i = 1; i <= rows; i++)
+    {
+        // Loop K - Something that creates empty spaces it takes I - rows.
+        for (k = 0; k < rows-i; k++)
+        printf(" ");
+        
+        // LOOP J - Inside of this loop J will print # and change line each time it is looped by the above function
+        for (j = 0; j < i; j++)   // Loops as long as J is bellow I
+        printf("#"); // Each time j cycles its one less # to print
+        
+        printf("  "); // Creates the spaces inbetween!!
+        
+        // LOOP J - creates the second part of the pyramid
+        for (j = 0; j < i; j++)   
+        printf("#");
+        
+        printf("\n"); // When all above loops are done, go to new line - and go back to LOOP I.
+    }
+    return 0;
 }
+    
+    
 
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-	// defines a integer names input - Can be used troughout code
-	int input;
-
-	{
-		// Asks for height of pyramid and stores it as "input"
-		input = get_int("Height: ");
-	}
-
-	// Says that input needs to be asked if input is below 1 or (||) above 23
-	while(input < 1 || input > 20)
-
-// Print function - Print this as long as this is true
-	{{
-		 // Starts a counter "i" where stuff repeats until this is true.
-		 for (int i = 0; i < input; input++);
-	 }
-	 printf("#"); }
-	printf("\n");
-stop
-}
