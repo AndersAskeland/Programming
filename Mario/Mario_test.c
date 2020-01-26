6f5c780d440f94d637636826298a4e76e804e235
AndersAskeland <anders_askeland@hotmail.com>
2020-01-26 14:24:15 +0100

    finished mario project

--- Mario/Mario_test.c
+++ Mario/Mario_test.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 main()
-{
+{ // define integers and ask for promt
 	int i;
 	int j;
 	int rows;
-	
-    do // This says that the scanf cannot recieve anything below 0
+  int k;
 
-    {
-        // Asks for a promt - Size
-		printf("Height: ");
-		scanf(" %d", &rows);    
-    } 
+
+  do // This says that the scanf cannot recieve anything below 0
+  {
+  // Asks for a promt - Size
+	printf("This is a Mario Pyramid builder!\nEnter a number betwen 1-23: ");
+	scanf(" %d", &rows);    
+  } 
     
-    	while (rows < 1 ); // Do the above as long as rows is below 1
+  while (rows < 1 || rows > 23); // Do the above as long as rows is bellow 1 or above 24
 
-// This creates a loop where 1 is added to i as long as i is bellow rows
-    for (i = 0; i < rows; i++)
+
+    // I LOOP - This creates a loop where 1 is added to i as long as i is below rows - Loops the entire bottom thing
+    for (i = 1; i <= rows; i++)
     {
-        // Inside of this loop J will start counting, and counting based on i
-
-        for (j = 0; j < i; j++)
-            { 
-            printf("#"); // Each time j cycles its one less # to print.
-            }
-            printf("\n");
+        // Loop K - Something that creates empty spaces it takes I - rows.
+        for (k = 0; k < rows-i; k++)
+        printf(" ");
+        
+        // LOOP J - Inside of this loop J will print # and change line each time it is looped by the above function
+        for (j = 0; j < i; j++)   // Loops as long as J is bellow I
+        printf("#"); // Each time j cycles its one less # to print
+        
+        printf("  "); // Creates the spaces inbetween!!
+        
+        // LOOP J - creates the second part of the pyramid
+        for (j = 0; j < i; j++)   
+        printf("#");
+        
+        printf("\n"); // When all above loops are done, go to new line - and go back to LOOP I.
     }
     return 0;
 }
