aba771cca86c6412b2ba0c0e9038d913becc5ce1
AndersAskeland <andersa_askeland@hotmail.com>
2020-02-03 22:02:09 +0100

    Tried doing some conversion of char to int -unsuccessful

--- CS50/Week_2/subsitution/substitution.c
+++ CS50/Week_2/subsitution/substitution.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) // Takes command line argument
+int main(int argc, char *argv[5]) // Takes command line argument - Argc is lenght of input, argV is input
 {
+    int cypher_id[5]; // A integer to store the int of the cypher in
+
+// Checks command line argument authentisity
     if (argc != 2)
     {
         printf("missing command-line argument\n");
+        return 1;
     }
     else if (argc == 2) // Should always be 2 because 
     {
@@ -16,5 +20,24 @@ int main(int argc, char *argv[]) // Takes command line argument
     {
         printf("Hello, world\n");
     }
-    return 0;
+
+// Get the int of each vector
+for (int i = 0; argv[i] = '\0'; i++)
+{
+    cypher_id[i] = &(*argv[i]);
+}
+    printf("Cypher_ID = %d\n", cypher_id[0]);
+    printf("Cypher_ID = %d\n", cypher_id[1]);
+    printf("Cypher_ID = %d\n", cypher_id[2]);
+    printf("Cypher_ID = %d\n", cypher_id[3]);
+    printf("Cypher_ID = %d\n", cypher_id[4]);
+
+// Changes normal characters to cypher
+//or (int j = 0; argv[j] = '\0'; j++)
+//{
+    /* code */
+// }
+
+
+    
 }
