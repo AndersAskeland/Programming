d09b455cedaef09c3f45fe5ff84a2086333b00d6
AndersAskeland <anders_askeland@hotmail.com>
2020-09-17 18:18:09 +0200

    finished the program without CS50. I forgot adding a null character to end of string

--- C/CS50/Week_2/subsitution/substitution.c
+++ C/CS50/Week_2/subsitution/substitution.c
@@ -1,29 +1,32 @@
-// A program to take a encryption key from the command line and substitute this with an input - Creating ciphertext
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// Take a encryption key from the command line and substitute this with an input - Creating ciphertext ////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Load packages
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <cs50.h>
 
 // function prototypes
 int duplicates(char string[]);
 int alp(char string[]);
 
-
 // main function
 int main(int argc, char *argv[1]) // Takes command line argument - Argc is lenght of input, argV is input
 {
-// Creates variables
-    char cypher_id[27]; // A integer to store the int of the cypher in
-    char cypher_id_upper[27];
-    char cypher_id_lower[27];
-    char input[27] = {0}; // Input from user to change
-    char output[27] = {0};
-    char alphabet[27];
-    char alphabet_upper[27];
-    int diff[27] = {0};
+    // Creates variables
+    char cypher_id[40]; // A integer to store the int of the cypher in
+    char cypher_id_upper[40];
+    char cypher_id_lower[40];
+    char input[99];
+    char output[40] = {0};
+    char alphabet[40];
+    char alphabet_upper[40];
+    int diff[40] = {0};
 
-
-// Validates command line argument
+    // Validates command line argument
     if (argc != 2) // Checks if there exists a commanbd line argument
     {
         printf("missing command-line argument\n");
@@ -36,11 +39,11 @@ int main(int argc, char *argv[1]) // Takes command line argument - Argc is lengh
     }
     else if (duplicates(argv[1]) != 0) // checks for duplicates
     {
-        return 1;    
+        return 1;
     }
-    else if (argc == 2) // Should always be 2 because 
+    else if (argc == 2) // Should always be 2 because
     {
-        printf("Sucess!Your code is %s\n", argv[1]);
+
     }
     else
     {
@@ -48,51 +51,46 @@ int main(int argc, char *argv[1]) // Takes command line argument - Argc is lengh
         return 1;
     }
 
-    
-// Store key in string
-    for (int i = 0; i < 26; i++) 
+
+    // Store key in string
+    for (int i = 0; i < 26; i++)
     {
-        cypher_id[i] = (argv[1][i]); 
+        cypher_id[i] = (argv[1][i]);
         cypher_id_lower[i] = tolower(cypher_id[i]); // lower case
         cypher_id_upper[i] = toupper(cypher_id[i]); // upper case
     }
-    printf("Cypher id lower is = %s\n", cypher_id_lower); // debug
-    printf("Cypher id upper is = %s\n", cypher_id_upper); // debug
-    
-// Checks if everything is alphabetic
+
+    // Checks if everything is alphabetic
     if (alp(cypher_id) != 0)
     {
         return 1;
     }
 
-// Asks for input - Probably need to add a do while loop here.
-    puts("plaintext:");
-    fgets(input, 27,stdin);
+    // Asks for input - Probably need to add a do while loop here.
 
-    printf("My word = %s\n", input); // Debug  
+    printf("plaintext: ");
+    fgets(input, sizeof(input), stdin);
 
-// Creates alphabet and 
-    for(int k = 0; k < 26; k++) // Creates the alphabeth - upper and lower
+    // Creates alphabet and
+    for (int k = 0; k < 26; k++) // Creates the alphabeth - upper and lower
     {
         alphabet[k] = 'a' + k; // I might need a null terminator
         alphabet_upper[k] = 'A' + k;
     }
 
 
-// Two loops that calculates output
-    for (int a = 0; input[a] !='\0'; a++) // First loop using input
+    // Two loops that calculates output
+    for (int a = 0; input[a] != '\0'; a++) // First loop using input
     {
-        if(islower(input[a])  || input[a] == ' ' || isdigit(input[a])) // checks if there is an lowercase
+        if (islower(input[a]) || input[a] == ' ' || input[a] == ',' || isdigit(input[a])) // checks if there is an lowercase
         {
-            printf("Running lowercase itteration %d\n\n", a);
-            for (int b = 0; alphabet[b] !='\0'; b++) // match input with alphabeth
+            for (int b = 0; alphabet[b] != '\0'; b++) // match input with alphabeth
             {
-                if (input[a] == alphabet[b] || input[a] == ' ' || isdigit(input[a]))
+                if (input[a] == alphabet[b] || input[a] == ',' || input[a] == ' ' || isdigit(input[a]))
                 {
-                    diff[a] = ((double)cypher_id_lower[b]-(double)alphabet[b]);
-                    printf("%c is %d different from %c\n", cypher_id_lower[b],diff[a] ,alphabet[b]);
+                    diff[a] = ((double)cypher_id_lower[b] - (double)alphabet[b]);
                     // Calculate output
-                    if (input[a] == ' ') // allows spaces
+                    if (input[a] == ' ' || input[a] == ',') // allows spaces
                     {
                         output[a] = (double)input[a];
                         break;
@@ -102,108 +100,104 @@ int main(int argc, char *argv[1]) // Takes command line argument - Argc is lengh
                         output[a] = (double)input[a];
                         break;
                     }
-                    
+
                     else if (diff > 0)
                     {
-                        output[a] = (double)input[a]+(double)diff[a];
-                        printf("lower output= %c\n", output[a]);
-                        break;                
+                        output[a] = (double)input[a] + (double)diff[a];
+                        break;
                     }
                     else
                     {
-                        output[a] = (double)input[a]-(double)diff[a];
-                        printf("lower output= %c\n", output[a]);
-                        break;                
+                        output[a] = (double)input[a] - (double)diff[a];
+                        break;
                     }
-                }                
+                }
             }
         }
-        else if(isupper(input[a])) // uppercase
+        else if (isupper(input[a])) // uppercase
         {
-            for (int b = 0; alphabet_upper[b] !='\0'; b++) // match input with alphabeth
+            for (int b = 0; alphabet_upper[b] != '\0'; b++) // match input with alphabeth
             {
                 if (input[a] == alphabet_upper[b] || input[a] == ' ')
                 {
-                    diff[a] = ((double)cypher_id_upper[b]-(double)alphabet_upper[b]);
-                    printf("%c is %d different from %c\n", cypher_id_upper[b],diff[a] ,alphabet_upper[b]);
+                    diff[a] = ((double)cypher_id_upper[b] - (double)alphabet_upper[b]);
                     // Calculate output
 
                     if (diff > 0)
                     {
-                        output[a] = (double)input[a]+(double)diff[a];
-                        printf("output= %c\n", output[a]);
-                        break;                
+                        output[a] = (double)input[a] + (double)diff[a];
+                        break;
                     }
                     else
                     {
-                        output[a] = (double)input[a]-(double)diff[a];
-                        printf("output= %c\n", output[a]);
-                        break;                
-                    }                
-                }        
+                        output[a] = (double)input[a] - (double)diff[a];
+                        break;
+                    }
+                }
             }
-   
+
         }
-        
+        // Add null character to end of string, so that program does not keep reading.
+        output[a+1] = '\0';
+
     }
 
-// Prints output
+    // Prints output
     printf("ciphertext: %s\n", output);
-    return 0; 
-} 
+    return 0;
+}
 
 
 
 
-// functions
-int alp(char string[]) // checks if everything is alphabetic
+
+
+
+/////////////////
+/// Functions ///
+/////////////////
+
+// Checks if alphabetic
+int alp(char string[])
 {
     for (int i = 0; string[i] != '\0'; i++)
     {
         if (isalpha(string[i]) == 0)
         {
             printf("key must only contain alphabetic characters.\n");
-            return 1;  
+            return 1;
         }
-
-        
-        
     }
     return 0;
 }
-int duplicates(char string[]) // check for duplicates
+
+// Checks for duplications
+int duplicates(char string[])
 {
-// defines variables
 // function - two loops
     for (int i = 0; i < (strlen(string)); i++)
-{
-    for (int j = i+1; j < (strlen(string)); j++) // lower to lower case
     {
-        if (string[i] == string[j] && string[j] != ' ')
+        for (int j = i + 1; j < (strlen(string)); j++) // lower to lower case
         {
-            printf("Code contains duplicates\n");
-            return 1;
-            break;  
+            if (string[i] == string[j] && string[j] != ' ')
+            {
+                printf("Code contains duplicates\n");
+                return 1;
+                break;
+            }
+            else if (string[i] == (string[j] - 32))
+            {
+                printf("Code contains duplicates lower - upper\n"); // I could also just had made everything lower case in main
+                return 1;
+                break;
+            }
+            else if (string[i] == (string[j] + 32))
+            {
+                printf("Code contains duplicates upper - lower\n");
+                return 1;
+                break;
+            }
         }
-        else if (string[i] == (string[j] - 32))
-        {
-            printf("Code contains duplicates lower - upper\n"); // I could also just had made everything lower case in main
-            return 1;
-            break;   
-        }
-        else if (string[i] == (string[j] + 32))
-        {
-            printf("Code contains duplicates upper - lower\n");
-            return 1;
-            break;  
-
-        }
-
-        
-        
-            
     }
-
-}  
-return 0;
+    return 0;
 }
