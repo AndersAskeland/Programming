c06d6c788436f0367958cb25b06ead6b63b37eff
AndersAskeland <anders_askeland@hotmail.com>
2020-09-19 18:53:06 +0200

    Save

--- C/CS50/Week_1/mario_less/mario.c
+++ C/CS50/Week_1/mario_less/mario.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 // Prototype
-void draw(int n);
+void draw(int n, int org);
+int input(void);
 
 
 // Main
@@ -12,29 +13,50 @@ int main(void)
     int *height = malloc(sizeof(int));
 
     // Gets user input
-    printf("Height: ");
-    scanf(" %d", height);
+    height = input();
 
     // Call function
-    draw(*height);
+    draw(*height, *height);
 }
 
 
-void draw(int n)
+void draw(int n, int org)
 {
     // Base case
-    if (n == 0) 
+    if (n == 0)
     {
         return;
     }
 
     // Recurssion
-    draw(n - 1);
+    draw(n - 1, org);
 
-    // Loop
+    // Space loop
+    for (int j = 0; j < (org - n); j++)
+    {
+        printf(" ");
+    }
+
+    // # loop
     for (int i = 0; i < n; i++)
     {
+
         printf("#");
     }
     printf("\n");
+}
+
+int input(void)
+{
+    // Variables
+    int *input = malloc(sizeof(int));
+    char term;
+
+    do
+    {
+        printf("Height: ");
+    }
+    while ((scanf("%d%c", input, &term) != 2 || term != '\n') || (*input < 1 && *input > 9));
+
+    return(*input);
 }
